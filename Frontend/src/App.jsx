1a07d0bdeeabb89b160fc05fe660fc00df06e447
Naomi <674259006@webmail.npru.ac.th>
2026-09-24 14:57:05 +0700

    first commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,21 +1,21 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Addproduct from "./pages/Addproduct";
-import Editproduct from "./pages/Editproduct";
-import Productform from "./pages/Productform";
-import productpage from "./pages/productpage";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Addproduct from "../pages/Addproduct";
+import Editproduct from "../pages/Editproduct";
+import Productform from "../pages/Productform";
+import Productpage from "../pages/Productpage";
 
-function App = () => {
+function App() {
   return (
     <BrowserRouter>
-     <Routes>
-       <Route path="/" element={<Navigate to="/productpage" />} />
-       <Route path="/productpage" element={<productpage />} />
-       <Route path="/addproduct/new" element={<Addproduct />} />
-       <Route path="/editproduct/:id" element={<Editproduct />} />
-       <route path="*" element={<Productform />} />
-     </Routes>
+      <Routes>
+        <Route path="/" element={<Navigate to="/productpage" replace />} />
+        <Route path="/productpage" element={<Productpage />} />
+        <Route path="/addproduct/new" element={<Addproduct />} />
+        <Route path="/editproduct/:id" element={<Editproduct />} />
+        <Route path="*" element={<Productform />} />
+      </Routes>
     </BrowserRouter>
   );
-};
+}
 
 export default App;
